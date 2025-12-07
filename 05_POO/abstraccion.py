bc996deb1d0bc9cf43eed2467b91e575b01e5768
Jenny213-mn <jc.manzanon@uea.edu.ec>
2025-12-07 16:49:00 -0500

    Agregar ejemplo de abstracción

---
+++ 05_POO/abstraccion.py
@@ -0,0 +1,20 @@
+# Ejemplo de ABSTRACCIÓN
+# Técnica: Abstracción
+# Autor: Jenny213-mn
+
+from abc import ABC, abstractmethod
+
+class Figura(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Cuadrado(Figura):
+    def __init__(self, lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado * self.lado
+
+figura = Cuadrado(5)
+print("Área del cuadrado:", figura.area())
